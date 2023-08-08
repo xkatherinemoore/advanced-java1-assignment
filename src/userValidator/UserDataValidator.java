7c333ba95e358ba7edcd5f57b0a29e512969a2ac
katherinemoore <katherinemoore789@gmail.com>
2023-08-07 18:31:57 -0700

    Created data validation program with I/O

--- src/userValidator/UserDataValidator.java
+++ src/userValidator/UserDataValidator.java
@@ -1,39 +1,61 @@
 package userValidator;
 
 import java.io.*;
+import java.util.Arrays;
 
 public class UserDataValidator {
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws IOException {
 		
-		// Inside a try block, instantiate 3 separate objects: a 'reader' that reads the
-		// data inside user_data.txt, a 'validWriter' for
-		// writing to a file with all the valid data,
-		// and an errorWriter for writing to a file with all the faulty data.
-		// If this try block fails, it would have to be because of an error from
-		// instantiating these readers (e.g. file not found, I/O exception, etc.), so
-		// just print the error to the console
+		BufferedReader dataReader = null;
+		BufferedWriter validWriter = null;
+		BufferedWriter errorWriter = null;
 		
-		
-			// Run a while loop that iterates through each line of the file
-
-
-				// In another try / catch block (inside the while loop), do the following:
-				// 1. Use the .split() method on the current line on "," so we can get an array
-				// of the name, email, and age of each user
-				// 2. If the length of the array is different than 3, we know something is wrong
-				// so throw an exception with message "Missing Data"
-				// 3. Save each piece of data in the array to its own variable, making sure to
-				// trim them of whitespace with .trim() and casting them to the correct datatype
-				// (Email + Name are strings, Age is int)
-				// 4. If age is less than or equal to 0, throw an exception with message
-				// "Invalid Age"
-				// 5. If the line passed all of these checks, write the line with the
-				// validWriter!
-				// 6. In the catch block, write the faulty line with the errorWriter appended
-				// with the error message!
-
-		// After the last catch statement, use a finally statement to close out of all
-		// of your readers and writers. This will require another try/catch block ;)
+		try {
+			dataReader = new BufferedReader(new FileReader("src/data/user_data.txt"));
+			validWriter = new BufferedWriter(new FileWriter("src/data/good_data.txt"));
+			errorWriter = new BufferedWriter(new FileWriter("src/data/bad_data.txt"));
+			
+			
+			String line;
+			while ((line = dataReader.readLine()) != null) {
+				
+				try {
+					String[] user = line.split(", ");
+					String name = user[0].trim();
+					String email = user[1].trim();
+					int age = Integer.parseInt(user[2].trim());
+					
+					if (user.length != 3) {
+						throw new ArrayIndexOutOfBoundsException("Data is missing");
+					}
+					
+					if (age <= 0) {
+						throw new NumberFormatException("Age is invalid");
+					}
+					
+					validWriter.write(line + "\n");
+				} catch(ArrayIndexOutOfBoundsException e) {
+					errorWriter.write("Missing Data: " + line + "\n");
+				} catch(NumberFormatException e) {
+					errorWriter.write("Invalid Age: " + line + "\n");
+				}
+				
+			}
+		} catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		} finally {
+			if (dataReader != null) {
+				dataReader.close();
+			}
+			if (validWriter != null) {
+				validWriter.close();
+			} 
+			if (errorWriter != null) {
+				errorWriter.close();
+			}
+		}
 		
 	}
 }
